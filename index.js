612ad2b1469d994e01c845991b521b9d646ef849
Fizashaikh11 <126874763+Fizashaikh11@users.noreply.github.com>
2024-08-22 22:08:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+<div class="main">
+          <div class="skillsContainer">
+                <h1>Skills</h1>
+                <div class="skillItem">
+                    <h2>Programming Language</h2>
+                    <p> 1. HTML</p>
+                </div>
+                <div class="skillItem">
+                    <p> 2. CSS</p>
+                </div>
+                <div class="skillItem">
+                    <p> 3. C </p>
+                </div>
+                <div class="skillItem">
+                    <p> 4. PHP </p>
+                </div>
+                <div class="skillItem">
+                    <p> 5. Java </p>
+                </div>
+                <div class="skillItem">
+                    <p> 6. JavaScript </p>
+                </div>
+            </div>
+
+
+            <div class="skillsContainer">
+                <h1>Certificate</h1>
+                <div class="skillItem">
+                    
+                    <p> 1. C Language</p>
+                </div>
+                <div class="skillItem">
+                    <p> 2. Java</p>
+                </div>
+                <div class="skillItem">
+                    <p> 3. Advanced CPP </p>
+                </div>    
+            </div>
+
+    
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.skillsItem {
+
+    font-family: "Nunito", sans-serif;
+    margin: 20px;
+}
+
+
+.skillsContainer h1{
+    font-family: "Nunito", sans-serif;
+    justify-content: center;
+    margin: 23px;
+}
+
+.skillsItem p {
+    font-family: "Source Code Pro", monospace;
+    margin: 23px;
+    justify-items: center;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+.contactform1{
+    padding: 133px;
+    background-color: #73605B;
+    text-align: center;
+    font-weight: bold;
+    padding-top: 0%;
+}
+
+.contactform1 h1{
+    align-items: center;
+    margin-top: 20px;
+    color: #330000;
+    padding: 40px;
+    font-weight: bolder;
+    font-size: 30pxpx;
+    padding-top: 1px;
+    max-width: fit-content;
+
+}
+
+.contactinfo{
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+    padding: 10px;
+    border: 1px solid#D09683;
+    border-radius: 10px;
+    background-color: #330000; 
+    width: 600px;
+}
